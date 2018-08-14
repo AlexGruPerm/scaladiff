25623d209da98dfb43ac85225ad1463d30124176
AlexGruPerm <ugr@bk.ru>
2018-08-14 17:15:08 +0500

    Now get tickers list and make calculation for all tickers with width_sec. Last steps: test write into res table. Move into main app.

--- src/First.scala
+++ src/First.scala
@@ -1,27 +1,18 @@
 import java.util.Date
 
 import bar.calculator.BarC
-//import bar.calculator.BarC
 import com.datastax.driver.core.{Row, Session}
 import org.slf4j.LoggerFactory
-
 import scala.collection.JavaConverters
 
 case class TendBarsMnMx(ticker_id : Int, width_sec : Int, deep_sec :Int, bars_cnt :Long, ts_begin :Long, ts_end :Long){
-  val interval_begin_end_sec = ts_end - ts_begin
-
-  val from_ts = ts_end - deep_sec
+  val intervalBeginEndSec = ts_end - ts_begin
+  val fromTs = ts_end - deep_sec
 
   override def toString = {
-    "ticker_id =" +ticker_id +" width_sec="+ width_sec +" deep_sec="+ deep_sec +" bars_cnt="+ bars_cnt +" ts_begin="+ ts_begin +" ts_end="+ ts_end +" interval_begin_end_sec="+ interval_begin_end_sec+ "sec.  SPLIT from_ts="+from_ts
+    "ticker_id =" +ticker_id +" width_sec="+ width_sec +" deep_sec="+ deep_sec +" bars_cnt="+ bars_cnt +" ts_begin="+ ts_begin +" ts_end="+ ts_end +" interval_begin_end_sec="+ intervalBeginEndSec+ "sec.  SPLIT from_ts="+fromTs
   }
-
 }
-
-
-
-
-
 
 
 object First extends App {
@@ -31,7 +22,16 @@ object First extends App {
 
   val session = client.session
 
-  val query_mnmx = """select count(ticker_id)  as bars_cnt,
+  val queryTickersWW =
+    """ select ticker_id
+          from mts_meta.bars_property
+         where bar_width_sec = 600 and
+               is_enabled    = 1
+         allow filtering; """
+
+  val prepqueryTickersWW = session.prepare(queryTickersWW)
+
+  val queryMinMax = """select count(ticker_id)  as bars_cnt,
                                min(ts_begin)    as ts_begin,
                                max(ts_end)      as ts_end
                         from mts_bars.bars
@@ -39,9 +39,9 @@ object First extends App {
                              bar_width_sec = :p_width_sec
                        allow filtering """
 
-  val prep_query_ = session.prepare(query_mnmx)
+  val prepQuery = session.prepare(queryMinMax)
 
-  val query_bars = """      select
+  val queryBars = """      select
                                   ticker_id,
                                   ddate,
                                   bar_width_sec,
@@ -65,7 +65,7 @@ object First extends App {
                                   ts_end       <= :p_ts_end
                              allow filtering;  """
 
-  val prep_query_bars = session.prepare(query_bars)
+  val prepQueryBars = session.prepare(queryBars)
 
 
   /**
@@ -76,17 +76,17 @@ object First extends App {
     * @param p_deep_sec
     * @return
     */
-  def get_bars_mnmx_tick_tend_1_conf(session: Session, p_ticker_id :Int, p_width_sec : Int, p_deep_sec :Int) : Seq[TendBarsMnMx] = {
-    val rowTo_TendBarsMnMx = (row: Row) => {
+  def getBarsMinMaxTickTendConf(session: Session, p_ticker_id :Int, p_width_sec : Int, p_deep_sec :Int) : Seq[TendBarsMnMx] = {
+    val rowToTendBarsMinMax = (row: Row) => {
       TendBarsMnMx(
         p_ticker_id, p_width_sec, p_deep_sec,
         row.getLong("bars_cnt"),
         row.getLong("ts_begin"),
         row.getLong("ts_end"))}
-    val bond_query_ =  prep_query_.bind().setInt("p_ticker_id", p_ticker_id)
+    val bond_query_ =  prepQuery.bind().setInt("p_ticker_id", p_ticker_id)
                                          .setInt("p_width_sec", p_width_sec)
     val rsTendBarsMnMx :Seq[TendBarsMnMx] = JavaConverters.asScalaIteratorConverter(session.execute(bond_query_).all().iterator())
-                                                   .asScala.toSeq.map(rowTo_TendBarsMnMx)
+                                                   .asScala.toSeq.map(rowToTendBarsMinMax)
     rsTendBarsMnMx
   }
 
@@ -103,7 +103,7 @@ object First extends App {
     * @param p_ts_end
     * @return
     */
-  def get_bars_by_ts_interval(session: Session, p_ticker_id :Int, p_width_sec : Int, p_ts_begin :Long, p_ts_end :Long) : Seq[BarC] = {
+  def getBarsByTsInterval(session: Session, p_ticker_id :Int, p_width_sec : Int, p_ts_begin :Long, p_ts_end :Long) : Seq[BarC] = {
     val rowToBar = (row : Row) => {
       new BarC(
         row.getInt("ticker_id"),
@@ -129,11 +129,11 @@ object First extends App {
     logger.debug("")
     logger.debug("---------------------------------------")
 
-    val bond_bars_query_ =  prep_query_bars.bind().setInt("p_ticker_id", p_ticker_id)
+    val bondBarsQuery =  prepQueryBars.bind().setInt("p_ticker_id", p_ticker_id)
                                                   .setInt("p_width_sec", p_width_sec)
                                                   .setLong("p_ts_begin",p_ts_begin)
                                                   .setLong("p_ts_end",p_ts_end)
-    val rsBars :Seq[BarC] = JavaConverters.asScalaIteratorConverter(session.execute(bond_bars_query_).all().iterator())
+    val rsBars :Seq[BarC] = JavaConverters.asScalaIteratorConverter(session.execute(bondBarsQuery).all().iterator())
       .asScala.toSeq.map(rowToBar).sortBy(t => t.ts_end)
     rsBars
   }
@@ -145,13 +145,13 @@ object First extends App {
     * @param p_index      - can be: 0,1,2 - only 3 parts
     * @param p_btype      - p_btype can be: g,r,n
     */
-  def get_btype_cnt_by_type_index(p_ticker :Int, p_seqBarInfo : List[(Int,Map[String,(Int,Double)])], p_index :Int, p_btype :String) = {
+  def getBtypeCntByTypeIndex(p_ticker :Int, p_seqBarInfo : List[(Int,Map[String,(Int,Double)])], p_index :Int, p_btype :String) = {
     val res = p_seqBarInfo.filter(sb => sb._1 == p_ticker)(p_index)._2.getOrElse(p_btype,(0,0.toDouble))._1
     logger.info(" INSIDE   []   :  p_ticker = "+p_ticker+"  index = "+p_index+" btype = "+p_btype+"   CNT = "+res )
     res
   }
 
-  def get_logCo_by_type_index(p_ticker :Int, p_seqBarInfo : List[(Int,Map[String,(Int,Double)])], p_index :Int, p_btype :String) = {
+  def getLogCoByTypeIndex(p_ticker :Int, p_seqBarInfo : List[(Int,Map[String,(Int,Double)])], p_index :Int, p_btype :String) = {
     val res = p_seqBarInfo.filter(sb => sb._1 == p_ticker)(p_index)._2.getOrElse(p_btype,(0,0.toDouble))._2
     logger.info(" INSIDE   []   :  p_ticker = "+p_ticker+"  index = "+p_index+" btype = "+p_btype+"   LOG_CO = "+res )
     res
@@ -164,11 +164,14 @@ object First extends App {
   //===================================================================================================================
   // read bar property and execute next for all bp.
 
+  val tickersWWRes =  JavaConverters.asScalaIteratorConverter(session.execute(prepqueryTickersWW.bind()).all().iterator())
+                     .asScala.toSeq.map(r => (r.getInt("ticker_id"),600,3*3600)).sortBy(_._1).toList
+
   val tendRes : Seq[TendBarsMnMx] = {for {
-                                          (l_ticker_id, l_width_sec, l_deep_sec) <- List((1,600, 3*3600),(2,600, 3*3600))
-                                          thisTendRes : TendBarsMnMx  <- get_bars_mnmx_tick_tend_1_conf(session, l_ticker_id, l_width_sec, l_deep_sec)
+                                          (lTickerId, l_width_sec, l_deep_sec) <- tickersWWRes  // List((1,600, 3*3600),(2,600, 3*3600))
+                                          thisTendRes : TendBarsMnMx  <- getBarsMinMaxTickTendConf(session, lTickerId, l_width_sec, l_deep_sec)
                                          } yield thisTendRes
-                                    }.filter(r => (r.interval_begin_end_sec >= r.deep_sec))
+                                    }.filter(r => (r.intervalBeginEndSec >= r.deep_sec))
 
   logger.info(" tendRes: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
   for (tr <- tendRes) logger.info(tr.toString)
@@ -176,7 +179,7 @@ object First extends App {
 
   val barsList : Seq[BarC] = for {
                                   barMnMx        <- tendRes
-                                  thisBar : BarC <- get_bars_by_ts_interval(session, barMnMx.ticker_id, barMnMx.width_sec, barMnMx.from_ts, barMnMx.ts_end)
+                                  thisBar : BarC <- getBarsByTsInterval(session, barMnMx.ticker_id, barMnMx.width_sec, barMnMx.fromTs, barMnMx.ts_end)
                                  } yield thisBar
 
   for (b <- barsList) logger.info("ticker_id="+b.ticker_id+" begin - end :    "+b.ts_begin+" - "+b.ts_end+"  ["+b.btype+"] "+b.log_co)
@@ -207,34 +210,33 @@ object First extends App {
 
   for (p <- seqSeqBars_Parts_AddInfo){
     logger.info(" [seqSeqBars_Parts_AddInfo]   ticker_id="+p._1+"  "+p.toString)
-
   }
 
   val wType = for (cticker <- seqSeqBars_Parts_AddInfo.map(bi => bi._1).distinct.sortBy(v => v)) yield {
       //1.Test on G.(Green - up bars)
     if (
-      (Seq(2, 3) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 0, "g")) &&
-        (Seq(3, 4) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "g")) &&
-        (Seq(4) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 2, "g"))
+      (Seq(2, 3) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 0, "g")) &&
+        (Seq(3, 4) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "g")) &&
+        (Seq(4) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 2, "g"))
         &&
-        (get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 2, "g") >
-          get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "g")) &&
-        (get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "g") >
-          get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 0, "g"))
+        (getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 2, "g") >
+          getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "g")) &&
+        (getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "g") >
+          getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 0, "g"))
     ) {
       logger.info("GREEN WAY")
       (cticker,"g")
     }
     //2. Test on R.(Red - down bars)
     else if (
-      (Seq(2, 3) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 0, "r")) &&
-        (Seq(3, 4) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "r")) &&
-        (Seq(4) contains get_btype_cnt_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 2, "r"))
+      (Seq(2, 3) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 0, "r")) &&
+        (Seq(3, 4) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "r")) &&
+        (Seq(4) contains getBtypeCntByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 2, "r"))
         &&
-        (get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 2, "r") <
-          get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "r")) &&
-        (get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 1, "r") <
-          get_logCo_by_type_index(cticker, seqSeqBars_Parts_AddInfo, 0, "r"))
+        (getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 2, "r") <
+          getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "r")) &&
+        (getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 1, "r") <
+          getLogCoByTypeIndex(cticker, seqSeqBars_Parts_AddInfo, 0, "r"))
     ) {
       logger.info("RED WAY")
       (cticker,"r")
